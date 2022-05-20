ba7f9da7da7fa07a8ad82d1d896bd31320e62b0a
juliafet <j.s.fetisova@gmail.com>
2022-05-20 14:49:08 +1000

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialExampleModule } from '../material.module';
+import { InputFormExample } from './input-form-example';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  declarations: [
+    InputFormExample
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
+    MatIconModule,
+  ],
+  providers: [],
+  bootstrap: [InputFormExample],
+})
+export class AppModule {}
